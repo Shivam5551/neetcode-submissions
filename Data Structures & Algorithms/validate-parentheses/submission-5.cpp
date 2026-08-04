d193822a0469130c405d0e9e9956e795f8df5312
Shivam Tiwari <139862414+Shivam5551@users.noreply.github.com>
2026-08-04 12:23:35 +0530

    Add: validate-parentheses - submission-5

---
+++ Data Structures & Algorithms/validate-parentheses/submission-5.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s.size() % 2 != 0) return false;
+        stack <char> st;
+        for(auto c: s) {
+            if(c == '{' || c == '[' || c == '(' ) 
+                st.push(c);
+            else {
+                if (st.empty()) return false;
+
+                char top = st.top();
+                st.pop();
+
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '['))
+                    return false;
+            }
+        }
+        return st.empty();
+    }
+};
